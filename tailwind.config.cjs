b0cfb46c4dd3be0859433575ec570625f725724e
KaloyanAnd <kaloyanand@gmail.com>
2025-05-28 23:36:05 +0300

    Deploy site

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,svelte,js,ts}',
+    './src/routes/**/*.{html,svelte,js,ts}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
